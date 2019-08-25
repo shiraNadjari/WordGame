fbe1a1bdfab083958baf62893328abb291ae30d7
Sara Mahfoda <saram058057>
2019-08-25 16:14:45 +0300

    added object image class

--- src/app/classes/Object.ts
+++ src/app/classes/Object.ts
@@ -1,3 +1,17 @@
 export class imageObject{
-    x: number;
+    id:number;
+    nane:string;
+    X1: number;
+    X2: number;
+    Y1: number;
+    Y2: number;
+    
+    constructor(id:number,n:string,x1:number,x2:number,y1:number,y2:number){
+        this.id=id;
+        this.nane=n;
+        this.X1=x1;
+        this.X2=x2;
+        this.Y1=y1;
+        this.Y2=y2;
+    }
 }
